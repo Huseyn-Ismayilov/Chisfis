aa359a77a05e7234e48d76c13c16d00065b65831
Huseyn-Ismayilov <info.cyweb@gmail.com>
2023-11-26 01:37:06 +0400

    #

--- assets/js/app.js
+++ assets/js/app.js
@@ -54,98 +54,6 @@ $('.page_overlay').click(function (e) {
 
 
 
-// 
-// 
-
-mobiscroll.setOptions({
-	theme: 'ios',
-	themeVariant: 'light'
-});
-
-mobiscroll.datepicker('#demo-responsive', {
-	controls: ['calendar'],
-	select: 'range',
-	showRangeLabels: true,
-	// responsive: {
-	//     xsmall: {
-	//         display: 'bottom'
-	//     },
-	//     small: {
-	//         display: 'anchored'
-	//     },
-	//     custom: { // Custom breakpoint
-	//         breakpoint: 800,
-	//         display: 'anchored',
-	//         touchUi: false
-	//     }
-	// }
-});
-
-
-$(function () {
-	$('#demo-item-templating').mobiscroll().select({
-		itemHeight: 40,
-		theme: 'ios',
-		themeVariant: 'light',
-		data: [{
-			text: 'Mercury – Act 1',
-			value: '56243',
-		}, {
-			text: 'Divide',
-			value: '18659',
-		}, {
-			text: 'Evolution',
-			value: '88459',
-		}, {
-			text: 'Future Nostalgia',
-			value: '65422',
-		}
-		],
-		renderItem: function (item) {
-			var data = item.data;
-			return '<div class="md-item-template">' +
-				'<div class="md-item-template-title">' +
-				'<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="h-4 w-4 sm:h-6 sm:w-6"><path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"></path></svg>' +
-				'<span>' + data.text + '</span>' +
-				'</div>' +
-				'</div>';
-		}
-	});
-});
-$(function () {
-	$('#demo-item-templating0').mobiscroll().select({
-		itemHeight: 40,
-		theme: 'ios',
-		themeVariant: 'light',
-		data: [{
-			text: 'Lorem ipsum',
-			value: '56243',
-		}, {
-			text: 'Lorem ipsum',
-			value: '18659',
-		}, {
-			text: 'Lorem ipsum',
-			value: '88459',
-		}, {
-			text: 'Lorem ipsum',
-			value: '65422',
-		}
-		],
-		renderItem: function (item) {
-			var data = item.data;
-			return '<div class="md-item-template">' +
-				'<div class="md-item-template-title">' +
-				'<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="h-4 w-4 sm:h-6 sm:w-6"><path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"></path></svg>' +
-				'<span>' + data.text + '</span>' +
-				'</div>' +
-				'</div>';
-		}
-	});
-});
-// End
-
-
-
 $(".c-theme-switch").on("click", () => {
 	$("html").toggleClass("dark_theme");
 	$("html").toggleClass("light_theme");
@@ -153,7 +61,7 @@ $(".c-theme-switch").on("click", () => {
 
 
 
-$('ul.tabs li').click(function () {
+$('.tabs li').click(function () {
 	var $this = $(this);
 	var $theTab = $(this).attr('id');
 	console.log($theTab);
@@ -167,29 +75,6 @@ $('ul.tabs li').click(function () {
 });
 
 
-
-$('#demo-booking-timegrid').mobiscroll().datepicker({
-	controls: ['calendar'],
-	max: '2024-05-17T00:00',
-	minTime: '08:00',
-	maxTime: '19:59',
-	stepMinute: 60,
-	labels: [{
-		start: '2023-11-17',
-		textColor: '#e1528f',
-		title: '2 SPOTS'
-	}],
-	invalid: [{
-		start: '2023-11-17T08:00',
-		end: '2023-11-17T13:00'
-	}, {
-		start: '2023-11-17T15:00',
-		end: '2023-11-17T17:00'
-	}, {
-		start: '2023-11-17T19:00',
-		end: '2023-11-17T20:00'
-	}]
-});
 
 
 var swiper = new Swiper(".mySwiper", {
@@ -242,28 +127,12 @@ on('click', '.option', item => {
 
 
 
-mobiscroll.datepicker('#picker', {
-	// controls: ['calendar'],
-	// select: 'range',
-	pages: 2,
-	// rangeHighlight: true,
-	// showRangeLabels: true,
-	// rangeStartHelp: 'Set dates',
-	// rangeEndHelp: 'Set dates'
-	controls: ['calendar'],
-	select: 'preset-range',
-	selectSize: 10,
-	// display: 'inline',
-	// touchUi: true
-});
-
 // mobiscroll.datepicker('#range', {
 //     controls: ['calendar'],
 //     select: 'range',
 //     calendarType: 'month',
 //     touchUi: true
 // });
-
 
 
 
@@ -278,14 +147,14 @@ $('.sub').click(function () {
 
 
 
-$(function () {
-	$('#value1, #value2, #value3').keyup(function () {
-		var value1 = parseFloat($('#value1').val()) || 0;
-		var value2 = parseFloat($('#value2').val()) || 0;
-		var value3 = parseFloat($('#value2').val()) || 0;
-		$('#sum').val(value1 + value2 + value3);
-	});
-});
+// $(function () {
+// 	$('#value1, #value2, #value3').keyup(function () {
+// 		var value1 = parseFloat($('#value1').val()) || 0;
+// 		var value2 = parseFloat($('#value2').val()) || 0;
+// 		var value3 = parseFloat($('#value2').val()) || 0;
+// 		$('#sum').val(value1 + value2 + value3);
+// 	});
+// });
 
 var swiper = new Swiper(".highlights_places .slider", {
 	slidesPerView: 2,
@@ -357,29 +226,37 @@ const largeSlider = () => {
 }
 window.addEventListener('load', largeSlider)
 
-// const buildSwiperSlider = sliderElm => {
-// 	const sliderIdentifier = sliderElm.dataset.id;
-// 	return new Swiper(`#${sliderElm.id}`, {
-// 		navigation: {
-// 			nextEl: `.swiper-button-next-${sliderIdentifier}`,
-// 			prevEl: `.swiper-button-prev-${sliderIdentifier}`
-// 		},
-// 		pagination: {
-// 			el: `.swiper-pagination-${sliderIdentifier}`,
-// 			type: 'progressbar',
-// 		},
-// 	});
-// }
-
-// // Get all of the swipers on the page
-// const allSliders = document.querySelectorAll('.listingCart .image_slider');
-
-// // Loop over all of the fetched sliders and apply Swiper on each one.
-// allSliders.forEach(slider => buildSwiperSlider(slider));
 
 
 
-// $(".c-theme-switch").on("click", () => {
-// 	$("html").toggleClass("dark_theme");
-// 	$("html").toggleClass("light_theme");
-// });
+$(".quantity_item .btn").click(function () {
+	$(this).parents().eq(0).toggleClass('active')
+});
+
+
+$(document).click(function (event) {
+
+	if (!$(event.target).closest(".quantity_item .btn, .quantity_item .quantity .dropdown").length) {
+		$("body").find(".quantity_item .quantity .dropdown").parents().eq(0).removeClass("active");
+	}
+
+});
+
+
+$(document).ready(function () {
+	$("#demo-item-templating0").asDatepicker({ mode: 'range' });
+});
+
+
+$(".search_item").click(function () {
+	$(this).toggleClass('active')
+	$(this).find('input').focus()
+	$(this).find('dropdown').toggleClass('opened')
+});
+
+
+$(document).click(function (event) {
+	if (!$(event.target).closest(".btn").length) {
+		$("body").find(".search_item").removeClass("opened");
+	}
+});
