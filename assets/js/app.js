fefaf56843007643c31784ebf8660f45724925bf
Huseyn-Ismayilov <info.cyweb@gmail.com>
2023-12-09 03:03:25 +0400

    #

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,17 +31,33 @@ var demo1 = new HotelDatepicker(
 
 // end
 
+function updateDarkModeState(isDarkMode) {
+	localStorage.setItem('dark-theme', isDarkMode ? '1' : '0');
+
+	var svgContent = isDarkMode
+		? `<svg xmlns="http://www.w3.org/2000/svg" width="28" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="w-7 h-7"><path fill-rule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69a.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" clip-rule="evenodd"></path></svg>`
+		: `<svg xmlns="http://www.w3.org/2000/svg" width="28" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="w-7 h-7"><path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"></path></svg>`;
+
+	localStorage.setItem('theme-svg-content', svgContent);
+}
+
+function applyDarkModeState() {
+	var wasDarkMode = localStorage.getItem('dark-theme') === '1';
+	$('html').toggleClass('dark-theme', wasDarkMode);
+
+	var svgContent = localStorage.getItem('theme-svg-content');
+	$('.c-theme-switch').html(svgContent);
+}
+
 $('.c-theme-switch').on('click', function () {
 	var wasDarkMode = localStorage.getItem('dark-theme') === '1';
-	localStorage.setItem('dark-theme', wasDarkMode ? '0' : '1');
-	$('html').toggleClass('dark-theme', !wasDarkMode);
-
-	$(this).toggleClass('active')
-	if ($(this).hasClass('active')) {
-		console.log("test");
-	}
+	updateDarkModeState(!wasDarkMode);
+	applyDarkModeState();
 });
 
+applyDarkModeState();
+
+// 
 
 $('.site_header .search_bar').click(function () {
 	$(this).next().toggleClass('opened');
